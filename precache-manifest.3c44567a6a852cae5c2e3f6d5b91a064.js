3e3a19f75a72e5356eb79648743b009e5415a3b2
vai91 <mehmetefeekiner@gmail.com>
2019-02-28 19:45:38 +0100

    Updates

---
+++ precache-manifest.3c44567a6a852cae5c2e3f6d5b91a064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f12d0e76de1c6eaecb",
+    "url": "/Book-Finder/static/css/main.bb815e44.chunk.css"
+  },
+  {
+    "revision": "63f12d0e76de1c6eaecb",
+    "url": "/Book-Finder/static/js/main.63f12d0e.chunk.js"
+  },
+  {
+    "revision": "6afe0313d4216da4a864",
+    "url": "/Book-Finder/static/js/runtime~main.6afe0313.js"
+  },
+  {
+    "revision": "906ea23539c0c5918ae3",
+    "url": "/Book-Finder/static/js/2.906ea235.chunk.js"
+  },
+  {
+    "revision": "ad6f66be76ea543a5186a84de58bdffa",
+    "url": "/Book-Finder/index.html"
+  }
+];
