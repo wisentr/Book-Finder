fe147feb6ea5432edd3d71cbcdfb51ad261ddf79
vai91 <mehmetefeekiner@gmail.com>
2019-03-04 20:45:31 +0100

    Updates

---
+++ precache-manifest.38f816bcf20f4d92f2579be65bb025ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf0d4b110bf27eb829e7",
+    "url": "/Book-Finder/static/css/main.88f7bc42.chunk.css"
+  },
+  {
+    "revision": "cf0d4b110bf27eb829e7",
+    "url": "/Book-Finder/static/js/main.cf0d4b11.chunk.js"
+  },
+  {
+    "revision": "6afe0313d4216da4a864",
+    "url": "/Book-Finder/static/js/runtime~main.6afe0313.js"
+  },
+  {
+    "revision": "348d2f1b64f7d25f86fb",
+    "url": "/Book-Finder/static/js/2.348d2f1b.chunk.js"
+  },
+  {
+    "revision": "9b115fb27dfaba994f89955c35fb4f90",
+    "url": "/Book-Finder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Book-Finder/precache-manifest.872dda3a29dcd2c09731fa1fcbb2467b.js"
+  "/Book-Finder/precache-manifest.38f816bcf20f4d92f2579be65bb025ae.js"
 );
 
 workbox.clientsClaim();
