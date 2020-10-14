3f9d5f8f6c05214c291c09057ebf485f8f2ff98f
Efe <mehmetefeekiner@gmail.com>
2020-10-14 14:19:31 +0200

    Updates

---
+++ precache-manifest.214e6ec3b0ba740d6fc28dc6882ebea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517588d5e13bd6d59b9889b9347f4d00",
+    "url": "/Book-Finder/index.html"
+  },
+  {
+    "revision": "93e20cb4ad6f34e1579d",
+    "url": "/Book-Finder/static/css/main.33534ce6.chunk.css"
+  },
+  {
+    "revision": "bf866e1533d1dee99aee",
+    "url": "/Book-Finder/static/js/2.46e6ecc0.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/Book-Finder/static/js/2.46e6ecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e20cb4ad6f34e1579d",
+    "url": "/Book-Finder/static/js/main.93db79c2.chunk.js"
+  },
+  {
+    "revision": "89b2a56e664410f461bd",
+    "url": "/Book-Finder/static/js/runtime-main.efb943d9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Book-Finder/precache-manifest.38f816bcf20f4d92f2579be65bb025ae.js"
+  "/Book-Finder/precache-manifest.214e6ec3b0ba740d6fc28dc6882ebea9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Book-Finder/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Book-Finder/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
